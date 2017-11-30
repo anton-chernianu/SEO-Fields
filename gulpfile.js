207e8a4d4454e173fe581f21957bc6970c72f990
Anton <anton@chernianu.com>
2017-11-30 10:17:36 +0200

    start project

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'), 
+    autoprefixer = require('gulp-autoprefixer'),
+    del = require('del'), 
+    pug = require('gulp-pug'), 
+    browserSync  = require('browser-sync'),
+
+    concat = require('gulp-concat'),
+    minifyJS = require('gulp-minify'),
+
+    uglify = require('gulp-uglifyjs'),
+    cssmin = require('gulp-cssmin'),
+    htmlmin = require('gulp-htmlmin'),
+
+    rename = require('gulp-rename')
+    ; 
+
+gulp.task('browser-sync', function() { 
+	browserSync({ 
+		server: { 
+			baseDir: 'src' 
+		},
+		notify: false 
+	});
+});
+
+gulp.task('pug', function() {
+  return gulp.src('src/pug/*.pug')
+      .pipe(pug({pretty: true}))
+      .pipe(gulp.dest('src'));
+});
+
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.scss')
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+    .pipe(gulp.dest('./src/css/'));
+});
+
+
+gulp.task('build', ['clean', 'sass'], function() {
+
+	var buildCss = gulp.src('src/css/**/*') 
+	.pipe(gulp.dest('build/css'))
+
+	var buildFonts = gulp.src('src/fonts/**/*') 
+	.pipe(gulp.dest('build/fonts'))
+
+	var buildJs = gulp.src('src/js/**/*') 
+	.pipe(gulp.dest('build/js'))
+
+	var buildHtml = gulp.src('src/*.html') 
+	.pipe(gulp.dest('build'));
+
+	var buildHtml = gulp.src('src/images/**/*') 
+	.pipe(gulp.dest('build/images'));
+
+});
+
+gulp.task('con-css', function() {
+  return gulp.src([
+  	// './src/css/test.css',
+  	'./src/css/main.css'
+  	 ])
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('con-js', function() {
+  return gulp.src([
+    // './src/js/test.js',
+  	'./src/js/main.js'
+  	 ])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('compress-js', function () {
+  gulp.src('dist/*.js')
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('compress-css', function () {
+    gulp.src('dist/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('htmlmin', function() {
+  return gulp.src('build/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('min'));
+});
+
+// gulp.task('compress', ['clean-min','con-css','con-js'], function() {
+// console.log('finish');
+// });
+
+// gulp.task('minify', ['compress-js', 'compress-css'], function() {
+// console.log('finish');
+// });
+
+gulp.task('clean', function() {
+	return del.sync('build'); 
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+  gulp.watch('src/sass/**/*.scss', ['sass']); 
+  gulp.watch('src/*.html', browserSync.reload); 
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+  gulp.watch('src/css/**/*.css', browserSync.reload);
+  gulp.watch('src/pug/**/*.pug', ['pug']); 
+});
+
+gulp.task('default', ['watch']);
---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+
+document.querySelector('.btn').addEventListener("click", function() {
+
+// Remove Popup Block
+  var oldPopup = document.querySelector('.seobookm');
+  if(oldPopup) {
+    oldPopup.outerHTML = "";
+    delete oldPopup;
+    console.log('remove old pop-up');
+  }
+
+// Create Popup
+  var body = document.body;
+  var d = document.createElement('div');
+
+  d.className='seobookm';
+  d.setAttribute("style", "position:fixed;top:0;left:0;width:100%;z-index:999;background-color:#fff;min-height:100px;border:2px solid #000;padding:5px;box-sizing:border-box;")
+  document.body.appendChild(d);
+  
+//   h1, h2
+  var h1 = document.querySelectorAll('h1'),
+      h2 = document.querySelectorAll('h2');
+
+// info block
+  var info = document.querySelector('.seobookm');
+  
+  for (var i=0; i < h1.length; i++) {
+    h1[i].setAttribute("style", "color:#fff; background-color:#173e43");
+  }
+  for (var i=0; i < h2.length; i++) {
+    h2[i].setAttribute("style", "color:#fff; background-color:#3fb0ac");
+  }
+
+// Get Title
+  var title = document.title;
+  
+// Get Meta
+  var meta = document.querySelector('meta[name="description"]').getAttribute('content');
+  
+  info.innerHTML = 
+    "Title: " + title + "<br>" +
+    "Description: " + meta;
+
+
+});
