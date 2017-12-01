0c49ea5d9b9a64f46afda6e54c0dadbe37bc84d5
Anton <anton@chernianu.com>
2017-12-01 16:58:41 +0200

    create css in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,92 @@
-document.querySelector('.btn').onclick = function(){
+document.querySelector('.runScript').onclick = function(){
+  
+  removeOldPopUp();
+  createCss();
+  createPopUp();
+  console.log('create');
 
-// Remove Popup Block
+};
+
+
+function createCss() {
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  var css = `
+  .seobookm {
+    font-family: 'Open Sans', sans-serif;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.7);
+    -webkit-transition: opacity 500ms;
+    transition: opacity 500ms;
+    visibility: visible;
+    opacity: 1;
+  }
+  .seobookm__content {
+    margin: 70px auto;
+    padding: 20px;
+    background: #fff;
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    width: 100%;
+    max-width: 980px;
+    position: relative;
+    -webkit-transition: all 5s ease-in-out;
+    transition: all 5s ease-in-out;
+  }
+  .seobookm__copy, .seobookm__text, .seobookm__title{
+    display:inline-block
+  }
+  .seobookm__title{
+    font-weight:700;
+    margin-top:15px;
+    margin-bottom:5px;
+  }
+  .seobookm__copy{
+    font-family:'Open Sans',sans-serif;
+    width:100%;
+    padding:5px;
+    resize:none
+  }
+  .close{text-align:right;cursor:pointer}
+  `;
+
+  style.type = 'text/css';
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+
+  head.appendChild(style);
+}
+
+function createPopUp() {
+  var body = document.body;
+  var mainSEOpopup = document.createElement('div');
+  mainSEOpopup.className = 'seobookm';
+
+  document.body.appendChild(mainSEOpopup);
+}
+
+function removeOldPopUp() {
+  // Remove Popup Block
   var oldPopup = document.querySelector('.seobookm');
   if(oldPopup) {
     oldPopup.outerHTML = "";
     delete oldPopup;
     console.log('remove old pop-up');
   }
+}
+
+document.querySelector('.btn').onclick = function(){
+
+
 
 // Create Popup
   var body = document.body;
