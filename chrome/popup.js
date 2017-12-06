e5ed7cd2a6f3bca38cff6de5d02f72027dafd075
Anton <anton@chernianu.com>
2017-12-06 15:34:47 +0200

    Function - Copy Input Text

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,13 +1,48 @@
- function popup() {
+// Send Message After Click
+function popup() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "start"});
    });
 }
+// Copy text from Input-Textarea
+function copyInputLeftClick(){
+
+  var inputCopy = document.querySelectorAll('.copy-text');
+  console.log(inputCopy);
+  inputCopy.forEach(item => {
+    item.addEventListener('click', (event)=>{
+
+      // var divInputCopy = document.querySelector('.input-copy');
+      // if (divInputCopy) {
+      //   divInputCopy.parentNode.removeChild(divInputCopy);
+      // }
+
+      event.target.select();
+      document.execCommand('copy');
+
+      // event.target.insertAdjacentHTML('afterend', '<div class="input-copy">copy</div>');
+    })
+  });
+
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("clickbtn").addEventListener("click", popup);
+    // Send Message After Click
+    document.getElementById("clickbtn").addEventListener("click", popup);
+
+    // Function Copy Text From Input-Textarea
+    copyInputLeftClick();
+
 });
+
+
+
+
+
+
+
+
 
 function onWindowLoad() {
     // execute script on page
